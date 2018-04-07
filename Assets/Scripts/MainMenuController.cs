c655045f643bc7088d3b7768dbba03e41d430929
Natasha Piminova <n.k.piminova@yandex.ru>
2018-04-07 21:42:38 +0400

    добавлено игровое меню

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("MiniGame");
+        Debug.Log("LoadScene(\"MiniGame\")");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,43 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityStandardAssets.Vehicles.Ball;
 
 public class PlayerController : BallUserControl
 {
-  public Text ScoreText;
+    public Text ScoreText;
 
-  private int _score;
+    private int _score;
 
-  private void Start()
-  {
-    _score = 0;
-    ScoreText.text = string.Format("Score: {0}", _score);
-  }
-
-  private void OnTriggerEnter(Collider other)
-  {
-    if (other.gameObject.CompareTag("Star"))
+    private void Start()
     {
-      OnStarCollided(other.gameObject);
-    } else if (other.gameObject.CompareTag("ZeroLevel")) {
-      OnZeroLevelCollided();
+        _score = 0;
+        ScoreText.text = string.Format("Score: {0}", _score);
     }
-  }
 
-  private void OnStarCollided(GameObject star)
-  {
-    star.SetActive(false);
-    _score += 1;
-    ScoreText.text = string.Format("Score: {0}", _score);
-  }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Star"))
+        {
+            OnStarCollided(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("ZeroLevel"))
+        {
+            OnZeroLevelCollided();
+        }
+    }
 
-  private void OnZeroLevelCollided()
-  {
-    var scene = SceneManager.GetSceneByName("MiniGame");
-    SceneManager.LoadScene(scene.buildIndex);
-  }
+    private void OnStarCollided(GameObject star)
+    {
+        star.SetActive(false);
+        _score += 1;
+        ScoreText.text = string.Format("Score: {0}", _score);
+    }
+
+    private void OnZeroLevelCollided()
+    {
+        // ХЗ почему игра не видит имя сцены. Если вывести его на консоль, то будет пустая строка. 
+        //var scene = SceneManager.GetSceneByName("Menu");
+        //SceneManager.LoadScene(scene.buildIndex);
+
+        var scene = SceneManager.GetSceneByBuildIndex(0);
+        Debug.Log("Need go to menu on buildScene " + scene.name);
+        SceneManager.LoadScene(0);
+    }
 
 }
